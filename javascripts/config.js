bce3e296ac6a24e305d78bf0da6665e9bf8f0600
kevx <kevx@kevx-mac.local>
2014-12-26 14:48:32 +0800

    addblog

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,7 +1,20 @@
-var CFG = {
+var RES = {
     'item_per_page': 1,
     'items': [
-        'hadoop_seqfile_append.md', 
-        'hive_on_yarn.md'
+        {
+            'file': 'java_invoke_chain.md',
+            'title': '',
+            'gmtCreate': '12/26/2014'
+        },
+        {
+            'file': 'hive_on_yarn.md',
+            'title': '',
+            'gmtCreate': '12/11/2014'
+        },
+        {
+            'file': 'hadoop_seqfile_append.md',
+            'title': '',
+            'gmtCreate': '12/02/2014'
+        }
     ]
 };
