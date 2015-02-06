7c006f9330973bf27069eaf90cdaab028e07f21e
kevx <kevx@kevx-mac.local>
2015-02-06 17:01:57 +0800

    add_blog

--- javascripts/config.js
+++ javascripts/config.js
@@ -4,17 +4,22 @@ var RES = {
         {
             'file': 'java_invoke_chain.md',
             'title': '',
-            'gmtCreate': '12/26/2014'
+            'gmtCreate': '2014/12/26'
         },
         {
             'file': 'hive_on_yarn.md',
             'title': '',
-            'gmtCreate': '12/11/2014'
+            'gmtCreate': '2014/12/11'
         },
         {
             'file': 'hadoop_seqfile_append.md',
             'title': '',
-            'gmtCreate': '12/02/2014'
+            'gmtCreate': '2014/12/02'
+        },
+        {
+            'file': 'services_center.md',
+            'title': '',
+            'gmtCreate': '2015/02/06'
         }
     ]
 };
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,57 @@
-console.log('This would be the main JS file.');
+console.log('loaded!');
+
+function sortByDate(a,b) {
+    return Date.parse(b['gmtCreate']) - Date.parse(a['gmtCreate']);
+}
+
+var h = location.search;
+var page = 1;
+var pagesz = RES.item_per_page;
+var items = RES.items.sort(sortByDate);
+var totalpage = Math.ceil(items.length / RES.item_per_page);
+
+if (h != null && h.length > 1) {
+    var ps = h.substring(h.indexOf("?page=")+6);
+    page = parseInt(ps);
+    if (pagesz * page > items.length) {
+        page = 1;
+    }
+}
+var start = (page - 1) * pagesz;
+var limit = Math.min(start + pagesz, items.length);
+
+for(var i = start; i < limit; i++) {
+    $.ajax({
+       url: 'markdown/' + items[i]['file'],
+       xhrFields: {
+          withCredentials: true
+       }
+    }).done(function( data ) {
+        var s = markdown.toHTML(data);
+        var div = '<div>' + s + '</div>';
+        $('#content').append(div);
+        $('pre code').each(function(i, block) {
+            hljs.highlightBlock(block);
+        });
+    });
+}
+
+$('#pagepre').click(function() {
+    var p = page - 1;
+    if (p <= 0) {
+        p = 1;
+        return;
+    }
+    location.replace('index.html?page=' + p);
+});
+
+$('#pagenxt').click(function() {
+    var p = page + 1;
+    if (p > totalpage) {
+        p = totalpage;
+        return;
+    }
+    location.replace('index.html?page=' + p);
+});
+
+$('#total').html(page + '/' + totalpage);
