9d0f7f107cfdb40e82c3fb4adaeedc869352eb47
zhen.xia <zhen.xia@tongdun.cn>
2017-12-08 18:12:03 +0800

    .

--- javascripts/config.js
+++ javascripts/config.js
@@ -2,57 +2,59 @@ var RES = {
     'item_per_page': 1,
     'items': [
         {
+            'id': 'aerospike-tutorial',
             'file': 'asp.md',
             'title': 'Aerospike数据库',
             'gmtCreate': '2017/11/30'
         },
         {
+            'id': 'java-invoke-chain',
             'file': 'java_invoke_chain.md',
             'title': 'Java调用链跟踪',
             'gmtCreate': '2014/12/26'
         },
         {
+            'id': 'duitang-base-arch',
             'file': 'dt_base_framework.md',
             'title': '技术体系切换与基础框架构建',
             'gmtCreate': '2015/01/10'
         },
         {
+            'id': 'hive-on-yarn',
             'file': 'hive_on_yarn.md',
             'title': 'Hive On Yarn',
             'gmtCreate': '2014/12/11'
         },
         {
+            'id': 'hadoop-seqfile-append',
             'file': 'hadoop_seqfile_append.md',
             'title': 'Hadoop文件追加模式',
             'gmtCreate': '2014/12/02'
         },
         {
+            'id': 'duitang-microservice',
             'file': 'services_center.md',
             'title': '堆糖服务化的一些记录',
             'gmtCreate': '2015/02/06'
         },
         {
+            'id': 'webapp-rpc-optimizing',
             'file': 'rt_optmizing.md',
             'title': 'Web应用响应平滑与RPC优化',
             'gmtCreate': '2015/03/16'
         },
         {
+            'id': 'duitang-dataplatform',
             'file': 'dt_dp.md',
             'title': '数据平台核心与规划',
             'gmtCreate': '2015/04/01'
         },
         {
+            'id': 'hdfs-atomic',
             'file': 'hdfs_atomic.md',
             'title': 'HDFS原子性与一致性',
             'gmtCreate': '2015/03/21'
         }
-
-    ]
-};
-
-var RES_ESTATE = {
-    'item_per_page': 1,
-    'items': [
     ]
 };
 
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,53 +1,59 @@
+//2017.12.08
+
 console.log('loaded!');
 
 var h = location.search;
-var page = 1;
-var pagesz = RES.item_per_page;
 var items = RES.items.sort(sortByDate);
-var totalpage = Math.ceil(items.length / RES.item_per_page);
+var prenum = 0;
+var currnum = 0;
+var nextnum = 0;
 
 if (h != null && h.length > 1) {
-    var ps = h.substring(h.indexOf("?page=")+6);
-    page = parseInt(ps);
-    if (pagesz * page > items.length) {
-        page = 1;
+    var item = null;
+    var id = h.substring(h.indexOf("?id=")+4);
+    for (var i = 0; i < items.length; i++) {
+        if (e.id == id) {
+            item = e;
+            currnum = i;
+            break;
+        }
     }
-}
-var start = (page - 1) * pagesz;
-var limit = Math.min(start + pagesz, items.length);
 
-for(var i = start; i < limit; i++) {
-    $.ajax({
-       url: 'data/' + items[i]['file'],
-       xhrFields: {
-          withCredentials: true
-       }
-    }).done(function( data ) {
-        var s = markdown.toHTML(data);
-        var div = '<div>' + s + '</div>';
-        $('#content').append(div);
-        $('pre code').each(function(i, block) {
-            hljs.highlightBlock(block);
+    if (currnum == items.length - 1) {
+        nextnum = currnum;
+    } else {
+        nextnum = currnum + 1;
+    }
+
+    if (currnum == 0) {
+        prenum = 0;
+    } else {
+        prenum = currnum + 1;
+    }
+
+    if (item != null) {
+        $.ajax({
+           url: 'data/' + items[currnum]['file'],
+           xhrFields: {
+              withCredentials: true
+           }
+        }).done(function( data ) {
+            var s = markdown.toHTML(data);
+            var div = '<div>' + s + '</div>';
+            $('#content').append(div);
+            $('pre code').each(function(i, block) {
+                hljs.highlightBlock(block);
+            });
         });
-    });
+    }
 }
 
 $('#pagepre').click(function() {
-    var p = page - 1;
-    if (p <= 0) {
-        p = 1;
-        return;
-    }
-    location.replace('index.html?page=' + p);
+    location.replace('index.html?id=' + items[nextnum]['id']);
 });
 
 $('#pagenxt').click(function() {
-    var p = page + 1;
-    if (p > totalpage) {
-        p = totalpage;
-        return;
-    }
-    location.replace('index.html?page=' + p);
+    location.replace('index.html?id=' + items[prenum]['id']);
 });
 
-$('#total').html(page + '/' + totalpage);
+$('#total').html(items.length);
